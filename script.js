d4e43b341c304b03f343f177c3023da4c7345611
Shiva <shivakmr2025@gmail.com>
2025-06-14 23:44:10 -0500

    darkmode button work

---
+++ script.js
@@ -0,0 +1,21 @@
+const darkModeButton = document.getElementById('darkModeButton');
+const Light = document.getElementById('light');
+const dark = document.getElementById('dark');
+
+//Dark mode toggle
+
+darkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('darkMode');
+
+// Changing the button icon 
+
+    if (document.body.classList.contains('darkMode')) {
+       Light.style.display='inline';
+       dark.style.display='none';
+    }
+    else {
+         Light.style.display='none';
+       dark.style.display='inline';
+    }
+
+});
